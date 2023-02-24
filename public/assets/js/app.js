6ed0ac200693c4ba290dee39dddbd9c2700d7d31
Maman` <75215687+JfeStudio@users.noreply.github.com>
2023-02-24 16:52:38 +0700

    detail|main content

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -37,4 +37,14 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   }
+
+  // xZoom
+  $(document).ready(function () {
+    $(".xzoom, .xzoom-gallery").xzoom({
+      zoomWidth: 500,
+      title: false,
+      tint: "#333",
+      Xoffset: 30,
+    });
+  });
 });
